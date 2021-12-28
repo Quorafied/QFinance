c4f906e7d4a23420b4cdb01bef365f1993fb7b71
Quorafied <79426523+Quorafied@users.noreply.github.com>
2021-12-28 14:57:05 +0000

    Initialize and start of database setup.

--- source/main.py
+++ source/main.py
@@ -1,11 +1,17 @@
-class something():
-    def __init__(self):
-        self.name = "nothing"
+from sqlDatabase import *
 
-    def changeName(self):
-        self.name = "this"
+# Instantiating a database interaction object.
+# This object will be used to connect, set up a cursor for and alter the content of the SQL localhost server.
+db = sqlDatabase()
 
-thisClass = something()
-thisClass.changeName()
+# This is used to connect to the sql localhost server.
+db.connect()
 
-print(thisClass.name)
+# This sets up a Cursor that will point and execute SQL Queries
+db.setCursor()
+
+# SQL Query - Creates a database called "QFinance"
+db.createDatabase("QFinance")
+
+# SQL Query - Point to a database to be altered by further actions
+db.useDatabase("QFinance")
