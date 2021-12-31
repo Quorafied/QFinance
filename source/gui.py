7b56f9ab569e203cfb6ec8ca413c3cf98c69fe93
Quorafied <79426523+Quorafied@users.noreply.github.com>
2021-12-31 15:57:15 +0000

    Store Object in GUI and Methods
    Added Objects to the GUI, however, Removing does not work yet.

--- source/gui.py
+++ source/gui.py
@@ -5,29 +5,56 @@ class GUI():
     def __init__(self):
         # Initiate the oject with empty Listbox.
         self.titles = []
+        self.objects = {}
 
-        self.layout = [[gui.Text('Search for titles')],
-                [gui.Input(size=(20, 1), enable_events=True, key='-INPUT-')],
+        self.layout = [[gui.Text('Search for titles'), gui.Text('Search for dates', pad=(50, 2))],
+                [gui.Input(size=(14, 1), enable_events=True, key='-TITLEINPUT-'), gui.Input(size=(14, 1), enable_events=True, key='-DATEINPUT-', pad=(40, 2))],
                 [gui.Listbox(self.titles, size=(20, 4), enable_events=True, key='-LIST-', expand_x=True)],
-                [gui.Button('Chrome'), gui.Button('Exit')]]
+                [gui.Button('Chrome'), gui.Button('Exit')],
+                [gui.Button("Remove")]
+                ]
 
-        self.window = gui.Window('QFinance - Main Page', self.layout, size=(200, 800), resizable=True, )
+        self.window = gui.Window('QFinance - Main Page', self.layout, size=(500, 400), resizable=True)
+    
+    # Append Object
+    def appendObj(self, obj):
+        self.objects[obj] = obj.t_id
+        self.titles.append(f"{obj.t_id} {obj.name}")
+    
+    # Remove Object from Listbox and Dictionary of the GUI
+    def removeObj(self, obj):
+        self.objects.remove(obj)
+        self.titles.remove(f"{obj.t_id} {obj.name}")
+
         
     # Event Loop
     def loopThrough(self):
         while True:
             event, values = self.window.read()
-            if event in (gui.WIN_CLOSED, 'Exit'):                # always check for closed window
+            if event in (gui.WIN_CLOSED, 'Exit'):
                 break
-            if values['-INPUT-'] != '':                         # if a keystroke entered in search field
-                search = values['-INPUT-']
+            if values['-TITLEINPUT-'] != '':                         # if a keystroke entered in search field
+                search = values['-TITLEINPUT-']
                 new_values = [x for x in self.titles if search in x]  # do the filtering
                 self.window['-LIST-'].update(new_values)     # display in the listbox
+            elif values['-DATEINPUT-'] != '':
+                search = values['-DATEINPUT-']
+                new_values = [x for x in self.titles if search in x]
+                self.window['-LIST-'].update(new_values)
             else:
                 # display original unfiltered list
                 self.window['-LIST-'].update(self.titles)
+
             # if a list item is chosen
             if event == '-LIST-' and len(values['-LIST-']):
-                gui.popup('Selected ', values['-LIST-'])
+                selected = values['-LIST-']
+                print(f"Selected : {selected}")
+                print(f"Titles available : {self.titles}")
+
+            # Removes object
+            if event == 'Remove':
+                self.titles.remove(selected)
+                self.objects.pop(selected[0][0])
+
 
         self.window.close()
--- source/main.py
+++ source/main.py
@@ -10,5 +10,15 @@ db.setCursor() # This sets up a Cursor that will point and execute SQL Queries
 db.createDatabase("QFinance") # SQL Query - Creates a database called "QFinance"
 db.useDatabase("QFinance")# SQL Query - Point to a database to be altered by further actions
 
+class oneObj():
+    def __init__(self):
+        self.name = "Barclays"
+        self.t_id = 1
+
+dasObj = oneObj()
+
 mainGUI = GUI()
+mainGUI.appendObj(dasObj)
+
 mainGUI.loopThrough()
+
